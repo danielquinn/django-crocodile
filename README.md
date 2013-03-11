2d9046ddde43f1f9bb5602b16434063b542bec07
Daniel Quinn <me@danielquinn.org>
2013-03-11 19:37:51 +0100

    Initial edit.  Not a whole lot of testing since I pulled it out of the primary project and dumped it into GitHub.

--- README.md
+++ README.md
@@ -2,3 +2,90 @@ django-crocodile
 ================
 
 A simple CSS and Javascript aggregator for django
+
+If you're looking for a simple way to aggregate all of your various style sheets into a single download, and do the same for your JavaScript files, this is a good start.  Here's how it works:
+
+This is what you probably have on your site.  If you don't, then you probably don't need an aggregator:
+
+    {% block css %}
+      <link rel="stylesheet" type="text/css" media="screen" href="{{ STATIC_URL }}appname/css/somefile.css" />
+      <link rel="stylesheet" type="text/css" media="screen" href="{{ MEDIA_URL }}path/to/something/else.css" />
+      <link rel="stylesheet" type="text/css" media="screen" href="https://www.somedomain.ca/path/to/remote/file.css" />
+      <style>
+        .classname {
+          background-image: url("awesome.png");
+        }
+      </style>
+    {% endblock css %}
+
+    {% block js %}
+      <script type="text/javascript" src="{{ STATIC_URL }}appname/js/somefile.js"></script>
+      <script type="text/javascript" src="{{ MEDIA_URL }}path/to/something/else.js"></script>
+      <script type="text/javascript" src="https://www.somedomain.ca/path/to/remote/file.js"></script>
+      <script>
+        alert("Keep being awesome!");
+      </script>
+    {% endblock js %}
+
+This isn't ideal, since you're left with multiple server hits, sometimes to remote servers.  In some of the more complex setups, your site could have 10 or even 20 CSS and/or JS files.  What's more, you probably have `{% block css %}` and `{% block js %}` subclassed elsewhere on your site, so this list of files is variable.
+
+Crocodile is setup with a simple template tag:
+
+    {% aggregate_css %}
+      {% block css %}
+        <link rel="stylesheet" type="text/css" media="screen" href="{{ STATIC_URL }}appname/css/somefile.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="{{ MEDIA_URL }}path/to/something/else.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="https://www.somedomain.ca/path/to/remote/file.css" />
+        <style>
+          .classname {
+            background-image: url("awesome.png");
+          }
+        </style>
+      {% endblock css %}
+    {% endaggregate_css %}
+
+    {% aggregate_js %}
+      {% block js %}
+        <script type="text/javascript" src="{{ STATIC_URL }}appname/js/somefile.js"></script>
+        <script type="text/javascript" src="{{ MEDIA_URL }}path/to/something/else.js"></script>
+        <script type="text/javascript" src="https://www.somedomain.ca/path/to/remote/file.js"></script>
+        <script>
+          alert("Keep being awesome!");
+        </script>
+      {% endblock js %}
+    {% endaggregate_js %}
+
+And the output looks something like this:
+
+    <script src="YOUR_MEDIA_URL/path/to/cached/file.js?release=YOUR_RELEASE_TAG" />
+
+The contents of `file.css` and `file.js` are the combined payloads of every file listed between the `{% aggregate_* %} and {% endaggregate_* %}` tags.  This will even include remote files and literal blocks if you put them in there.
+
+It's entirely possible that you may not want all of these files to be loaded at once, as in cases where you may want to force the remote loading of come files.  To do that, you just keep those definitions out of the aggregate block:
+
+    {% aggregate_css %}
+      {% block css %}
+        <link rel="stylesheet" type="text/css" media="screen" href="{{ STATIC_URL }}appname/css/somefile.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="{{ MEDIA_URL }}path/to/something/else.css" />
+        <style>
+          .classname {
+            background-image: url("awesome.png");
+          }
+        </style>
+      {% endblock css %}
+    {% endaggregate_css %}
+
+    {% block remote_css %}
+      <link rel="stylesheet" type="text/css" media="screen" href="https://www.somedomain.ca/path/to/remote/file.css" />
+    {% endblock remote_css %}
+
+Everything outside of the aggregation block is left alone.
+
+## TODO
+
+* Medium-aware CSS is still sketchy.  Basically it currently grabs all CSS files that aren't set to `media="print"` and dumps them into the aggregated `.css` file.  If you have a printable .css file, it's best to keep it out of your aggregation block for this reason.
+  * Other media types are just rolled into the aggregated file, so that may cause some headaches.
+* Exploder-specific CSS tags (`<!-- if IE lt 8>`) are also ignored.  Ew.
+
+This is all still pretty new, so use at your own risk.
+
---
+++ crocodile/templatetags/crocodile.py
@@ -0,0 +1,232 @@
+import os, re
+
+from abc import ABCMeta, abstractmethod
+from hashlib import md5
+from urllib import quote
+from urllib2 import urlopen, urlparse
+
+from django import template
+from django.conf import settings
+from django.contrib.staticfiles.finders import FileSystemFinder, AppDirectoriesFinder
+
+register = template.Library()
+
+@register.tag
+def aggregate_js(parser, token):
+    """
+    """
+    nodelist = parser.parse(('aggregate_js',))
+    parser.delete_first_token()
+
+    return JavascriptNode(nodelist)
+
+
+
+@register.tag
+def aggregate_css(parser, token):
+    """
+    """
+    nodelist = parser.parse(('aggregate_css',))
+    parser.delete_first_token()
+
+    return CSSNode(nodelist)
+
+
+
+class StaticfileNode(template.Node):
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, nodelist):
+        self.type = ""
+        self.nodelist = nodelist
+
+
+    def _detect_enabled(self):
+
+        if hasattr(settings, "ENABLE_AGGREGATION"):
+            return settings.ENABLE_AGGREGATION
+        else:
+            return not settings.DEBUG
+
+
+    def render(self, context):
+
+        source_markup = self.nodelist.render(context)
+
+        if not self._detect_enabled():
+            return source_markup
+
+        cache_filename = os.path.join(
+            settings.MEDIA_ROOT,
+            "cache",
+            self.type,
+            "%s.%s" % (
+                md5(settings.RELEASE + source_markup).hexdigest(),
+                self.type
+            )
+        )
+
+        if settings.DEBUG or not os.path.exists(cache_filename):
+
+            output = self._compile(context)
+            output = self._compress(output)
+
+            try:
+                os.makedirs(os.path.dirname(cache_filename))
+            except OSError:
+                pass
+
+            open(cache_filename, "w").write(output.encode("utf-8"))
+
+        return self._markup(cache_filename.replace(
+            settings.MEDIA_ROOT,
+            settings.MEDIA_URL
+        ))
+
+
+    def _getfile (self, filename):
+
+        r = FileSystemFinder().find(filename)
+
+        if not r:
+            r = AppDirectoriesFinder().find(filename)
+
+        if not r and filename.startswith("http"):
+            return self._fetch_url(filename)
+
+        if not r:
+            return "<!-- FILE NOT FOUND: %s -->\n" % filename
+
+        return open(r).read().decode("utf-8", "replace") + "\n"
+
+
+    @abstractmethod
+    def _compile(self, context):
+        pass
+
+
+    @abstractmethod
+    def _compress(self, uncompressed):
+        pass
+
+
+    @abstractmethod
+    def _markup(self, f):
+        pass
+
+
+    def _fetch_url(self, url):
+        return ""
+
+
+
+class JavascriptNode(StaticfileNode):
+
+    def __init__(self, *a, **kwa):
+        super(JavascriptNode, self).__init__(*a, **kwa)
+        self.type = "js"
+
+
+    def _compile(self, context):
+
+        js = unicode()
+        for line in self.nodelist.render(context).split("\n"):
+
+            path = re.match(r"^.*<script.*src=.([^'\"]+).*$", line) # Import js files
+            ignore = re.match(r"^.*</?script.*", line)              # Ignore the <script> tags when we're writing straight into the file
+
+            if path:
+                js += self._getfile(path.group(1).replace(settings.STATIC_URL, ""))
+            elif not ignore:
+                js += line + "\n"
+
+        return js
+
+
+    def _compress(self, uncompressed):
+        return uncompressed
+
+
+    def _markup(self, file_contents):
+        return '<script language="javascript" src="%s?release=%s"></script>' % (
+            file_contents,
+            quote(settings.RELEASE)
+        )
+
+
+
+class CSSNode(StaticfileNode):
+
+    _fetch_regex = re.compile("url\(\"?'?(.*?)\"?'?\)")
+
+    def __init__(self, *a, **kwa):
+        super(CSSNode, self).__init__(*a, **kwa)
+        self.type = "css"
+
+
+    def _determine_file_list(self):
+        pass
+
+
+    def _compile(self, context):
+
+        css = ""
+
+        comment = False
+        for line in self.nodelist.render(context).split("\n"):
+
+            if not comment and "<!--" in line:
+                comment = True
+
+            if comment:
+                if "-->" in line:
+                    comment = False
+                continue
+
+            source = re.match(r"^.*<link.*href=.([^'\"]+).*$", line) # Import css files
+            ignore = re.match(r"^.*</?style.*", line)                # Ignore the <style> tags when we're writing straight into the file
+
+            if source:
+                if not re.search(r"media=('|\")?print('|\")?", line):
+                    source = source.group(1).replace(settings.STATIC_URL, "")
+                    css += self._getfile(source).replace("{{ STATIC_URL }}", settings.STATIC_URL)
+            elif not ignore:
+                css += line + "\n"
+
+        return css
+
+
+    def _compress(self, uncompressed):
+        return uncompressed
+
+
+    def _markup(self, file_contents):
+        return '<link rel="stylesheet" href="%s?release=%s" type="text/css" />' % (
+            file_contents,
+            quote(settings.RELEASE)
+        )
+
+
+    def _fetch_url(self, url):
+        """
+        Assuming the remote URLs contain reasonably-formed CSS using url()
+        where appropriate, rework the paths to use complete URLs instead of
+        relative ones.
+        """
+
+        url = urlparse.urlparse(url)
+        return re.sub(
+            self._fetch_regex,
+            lambda m: "url('%s://%s%s')" % (
+                url.scheme,
+                url.netloc,
+                os.path.normpath(
+                    os.path.join(
+                        os.path.dirname(url.path),
+                        m.group(1)
+                    )
+                )
+            ),
+            urlopen(url.geturl()).read().decode("utf-8")
+        )
