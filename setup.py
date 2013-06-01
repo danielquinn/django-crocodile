f04927d2a8748d2358777d23ae42a754d9552c27
Daniel Quinn <user@server.fake>
2013-06-01 11:41:56 +0200

    Getting ready to push to pypi

--- setup.py
+++ setup.py
@@ -1,58 +1,30 @@
 import os
-from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
+from setuptools import setup
 
-app_name = 'crocodile'
-version = __import__(app_name).__version__
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk(app_name):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[len(app_name)+1:] # Strip "app_name/" or "app_name\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
+# Allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-' + app_name,
-    version=version,
-    description='A simple CSS and Javascript aggregator for your Django site.',
-    long_description=open('README.md').read(),
-    author='Daniel Quinn',
-    author_email='me@danielquinn.org',
-    url="https://github.com/danielquinn/django-crocodile",
-    package_dir={app_name: app_name},
-    packages=packages,
-    package_data={app_name: data_files},
+    name="django-crocodile",
+    version="0.1",
+    packages=["crocodile"],
+    include_package_data=True,
+    description="A simple Django app to conduct Web-based polls.",
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
+    url="https://github.com/danielquinn/django-crocodile/",
+    author="Daniel Quinn",
+    author_email="me@danielquinn.org",
     install_requires=["cssmin",],
     classifiers=[
-        'Development Status :: 5 - Production/Unstable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: GPL-3',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Utilities'
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    zip_safe=False,
 )
-
